b68ded0904de14eff1e3bd4679d64bea06a29b06
Brad Westfall <brad@builtwithweb.com>
2024-09-12 11:24:01 -0400

    small fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,34 +5,12 @@ import { Slash, SlashClip, Slashes } from '~/components/Slashes'
 export default function Index() {
   return (
     <>
-      {/* <header className="fixed z-50 left-0 right-0 top-16 ">
-        <CenterContent>
-          <div className="flex gap-6 max-lg:flex-col">
-            <div className="lg:flex-1">left</div>
-            <nav>
-              <a href="/" className="inline-block px-6 py-3">
-                Link
-              </a>
-              <a href="/" className="inline-block px-6 py-3">
-                Link
-              </a>
-              <a href="/" className="inline-block px-6 py-3">
-                Link
-              </a>
-              <a href="/" className="inline-block px-6 py-3">
-                Link
-              </a>
-            </nav>
-          </div>
-        </CenterContent>
-      </header> */}
-
       <section className="hero absolute inset-0 overflow-hidden z-10">
         <div className="hero-border"></div>
 
         <SlashClip className="hero-slash-background from-radial-center to-radial-edge" />
 
-        <div className="w-96 m-16">
+        <div className="hero-logo">
           <Logo fill="white" />
         </div>
 
@@ -60,12 +38,12 @@ export default function Index() {
 
       <section className="fixed inset-0">
         <div className="absolute top-0 -left-[300px] w-[1050px] -rotate-[62.3deg]">
-          <Logo fill="black" />
+          <Logo fill="white" />
         </div>
       </section>
 
-      {/* <section className="absolute inset-0 h-[200vh]">
-        <div className="sticky top-0 w-screen h-screen bg-amber-100">
+      <section className="absolute inset-0 h-[210vh]">
+        <div className="sticky inset-[40px] w-screen h-screen bg-black text-white">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe atque deleniti molestiae ullam esse ratione,
           perspiciatis excepturi cum dolorum quisquam repellendus. Inventore doloremque explicabo ducimus dolores fugiat
           odio corporis debitis.
@@ -74,9 +52,9 @@ export default function Index() {
           <br />
         </div>
         <div className="w-screen h-screen" aria-hidden />
-      </section> */}
+      </section>
 
-      <div className="h-[2000px] bg-white" />
+      <div className="h-[10000px] bg-white" />
     </>
   )
 }
--- app/routes/play.tsx
+++ app/routes/play.tsx
@@ -2,7 +2,13 @@ export default function Play() {
   return (
     <div className="h-[2000px]">
       <div className="w-52 h-[200px] bg-sky-700">
-        <div className="h-[100px] bg-red-500 sticky top-0"></div>
+        <div className="h-[100px] bg-white/20 sticky top-0">page</div>
+      </div>
+      <div className="w-52 h-[200px] bg-red-700">
+        <div className="h-[100px] bg-white/20 sticky top-0">page</div>
+      </div>
+      <div className="w-52 h-[200px] bg-purple-700">
+        <div className="h-[100px] bg-white/20 sticky top-0">page</div>
       </div>
     </div>
   )
