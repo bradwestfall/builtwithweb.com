585828e8318a090431d3247f3cf75b3c7e74c04c
Brad Westfall <brad@builtwithweb.com>
2024-09-21 16:59:21 -0400

    latest

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,17 +5,19 @@ import { Logo } from '~/components/Logo'
 import { Slashes } from '~/components/Slashes'
 
 export default function Index() {
+  const style = { '--mask-container': 'var(--hero-container)' } as React.CSSProperties
+
   return (
     <>
       <div className="main-border" aria-hidden />
 
-      <section className="hero absolute viewport-container z-[10]">
+      <section className="hero section-mask-timeline-container viewport-container absolute z-[10]">
         <div className="text-white w-32 absolute z-20 top-7 mx-auto left-7 right-7 md:right-auto">
           <Logo fill />
         </div>
 
-        <div className="hero-mask text-white">
-          <div className="hero-mask-child">
+        <div className="section-mask text-white">
+          <div className="section-mask-inner">
             <nav className="primary-nav text-lg absolute top-[10px] left-[10px] right-[10px] mx-auto w-fit md:left-auto">
               <NavLink to="/" className="inline-block px-3 py-2 sm:px-6 sm:py-3 text-inherit">
                 Home
@@ -55,8 +57,8 @@ export default function Index() {
         </div>
       </section>
 
-      <section className="services-sticky-wrap absolute z-[9] inset-0 h-[150vh] md:h-[190vh] overflow-clip">
-        <div className="services-body sticky viewport-container flex flex-col">
+      <section className="services section-mask-timeline-container absolute z-[9] inset-0 h-[150vh] md:h-[190vh] overflow-clip">
+        <div className="sticky viewport-container flex flex-col">
           <nav className="glimmer-border-mask absolute text-lg top-[10px] left-[10px] right-[10px] mx-auto w-fit md:left-auto">
             <div className="nav-wrap">
               <NavLink to="/" className="inline-block px-3 py-2 sm:px-6 sm:py-3 text-theme-black">
@@ -74,43 +76,41 @@ export default function Index() {
             </div>
           </nav>
 
-          <div className="services-logo text-theme-black">
-            <Logo fill />
-          </div>
+          <div className="section-mask bg-white">
+            <div className="section-mask-inner">
+              <div className="services-logo text-theme-black">
+                <Logo fill />
+              </div>
+              <div className="flex flex-col justify-between max-w-[900px] overflow-y-scroll">
+                {/* <div className="glimmer-text absolute top-16 lg:-top-5 left-0 max-w-[800px]"> */}
+                <div className="glimmer-text leading-none mt-16">
+                  <span className="text-nowrap leading-none">Web Apps</span>
+                  <p className="pt-[0.2em] text-[0.5em] lg:text-[0.32em] leading-normal">
+                    Your project deserves experience and quality. My experience building web sites goes back 20 years
+                    and I teach React to Adobe, Microsoft, Apple, and many more.
+                  </p>
+                </div>
 
-          <div className="flex-1 flex flex-col justify-between max-w-[900px] overflow-y-scroll">
-            {/* <div className="glimmer-text absolute top-16 lg:-top-5 left-0 max-w-[800px]"> */}
-            <div className="glimmer-text leading-none mt-16">
-              <span className="text-nowrap leading-none">Web Apps</span>
-              <p className="pt-[0.2em] text-[0.5em] lg:text-[0.32em] leading-normal">
-                Your project deserves experience and quality. My experience building web sites goes back 20 years and I
-                teach React to Adobe, Microsoft, Apple, and many more.
-              </p>
-            </div>
-
-            {/* <div className="absolute bottom-5 left-1 right-1 md:right-auto md:w-[calc(40vw+200px)] xl:text-xl flex gap-10 md:gap-4 max-md:flex-col"> */}
-            <div className="xl:text-xl flex gap-10 md:gap-4 max-md:flex-col mb-5">
-              <ServiceCard label="Design">
-                Capture your audience with beautiful, accessible, amazing designs. It's the first impression that
-                counts.
-              </ServiceCard>
-              <ServiceCard label="Build">JavaScript powers the web. Let's build...</ServiceCard>
-              <ServiceCard label="Teach">
-                Level up your team with expert knowledge. I've taught some of the biggest tech companies JavaScript,
-                React, and CSS.
-              </ServiceCard>
+                {/* <div className="absolute bottom-5 left-1 right-1 md:right-auto md:w-[calc(40vw+200px)] xl:text-xl flex gap-10 md:gap-4 max-md:flex-col"> */}
+                <div className="xl:text-xl flex gap-10 md:gap-4 max-md:flex-col mb-5">
+                  <ServiceCard label="Design">
+                    Capture your audience with beautiful, accessible, amazing designs. It's the first impression that
+                    counts.
+                  </ServiceCard>
+                  <ServiceCard label="Build">JavaScript powers the web. Let's build...</ServiceCard>
+                  <ServiceCard label="Teach">
+                    Level up your team with expert knowledge. I've taught some of the biggest tech companies JavaScript,
+                    React, and CSS.
+                  </ServiceCard>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="next mt-[150vh] md:mt-[190vh] overflow-clip">
-        <div className="sticky viewport-container">
-          Hello...
-          {/* <div className="absolute -left-[300px] w-[1050px] -rotate-[62.3deg] z-10">
-            <Logo fill />
-          </div> */}
-        </div>
+      <section className="next section-mask-timxxxxxxeline-container absolute z-[8] inset-0 h-[390vh] overflow-clip">
+        <div className="sticky viewport-container ">Hello</div>
       </section>
 
       {/* <section className="absolute z-[1] h-full inset-0 bg-gradient-to-tr from-slate-200 to-slate-300">
@@ -131,7 +131,7 @@ function ServiceCard({ children, label }: { children: React.ReactNode; label: st
     'after:h-[0.3em] after:absolute after:top-0 after:left-[6em] md:after:left-[8em] after:-right-0 after:bg-theme-black after:rounded'
 
   return (
-    <div className="flex-1 flex relative bg-white" style={{ boxxShadow: '0 0 0px 10px white' }}>
+    <div className="flex-1 flex relative">
       <div className="-translate-y-[0.4em] text-[1.4em] md:text-[2em] absolute leading-none">{label}</div>
       <div className={`border-b-[0.3em] border-r-[0.3em] border-theme-black overflow-clip ${before} ${after}`}>
         <p className="p-[1.2em] pt-[1.8em] text-[1em]">{children}</p>
